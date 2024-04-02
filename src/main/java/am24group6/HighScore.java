e1dfc95326f45ec4c4bb6efefb5a2eb1005f60ca
helben2002 <helben2002@skola.goteborg.se>
2024-04-02 20:03:00 +0200

    refactoring highscore-class

--- src/main/java/am24group6/HighScore.java
+++ src/main/java/am24group6/HighScore.java
@@ -7,28 +7,25 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class HighScore {
-    private static final String fileEasy = "highscoreEasy.txt";
-    private static final String fileHard = "highscoreHard.txt";
+    private static final String FILE_EASY = "highscoreEasy.txt";
+    private static final String FILE_HARD = "highscoreHard.txt";
 
-    public static void saveHighscore(double score, boolean highScoreLevel){
-        try (FileWriter wr = new FileWriter(highScoreLevel ? fileEasy : fileHard)) {
-            wr.write(Integer.toString((int) score));            
+    public static void saveHighscore(double score, boolean highScoreLevel) {
+        try (FileWriter wr = new FileWriter(highScoreLevel ? FILE_EASY : FILE_HARD)) {
+            wr.write(Integer.toString((int) score));
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    public static int getHighScore(boolean highScoreLevel){
+    public static int getHighScore(boolean highScoreLevel) {
         int highScore = 0;
-        try {
-            File file = new File(highScoreLevel ? fileEasy : fileHard);
-            if (!file.exists()){
+        try (BufferedReader reader = new BufferedReader(new FileReader(highScoreLevel ? FILE_EASY : FILE_HARD))) {
+            File file = new File(highScoreLevel ? FILE_EASY : FILE_HARD);
+            if (!file.exists()) {
                 return 0;
             }
-
-            BufferedReader reader = new BufferedReader(new FileReader(highScoreLevel ? fileEasy : fileHard));
             highScore = Integer.parseInt(reader.readLine());
-            reader.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
